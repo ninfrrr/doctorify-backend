95528d13e0d4b99c6b614c84ab0d60cb798e2d4f
Luthfi Izzuddin Hanif <izzuddin.hanif29@gmail.com>
2023-05-06 14:58:04 +0700

    fix(jetstream): reinstall jetstream with livewire

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\SpecialistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Auth API
+Route::name('auth.')->group(function () {
+    Route::post('login', [UserController::class, 'login'])->name('login');
+    Route::post('register', [UserController::class, 'register'])->name('register');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [UserController::class, 'logout'])->name('logout');
+        Route::get('user', [UserController::class, 'fetch'])->name('fetch');
+    });
+});
+
+// Specialist API
+Route::prefix('specialist')->name('specialist')->group(function () {
+    Route::post('', [SpecialistController::class, 'create']);
 });
